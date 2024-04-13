36eafe587ba60d96d67aee6ae1b2c2198f67d364
wajji01 <wajahatmustafa725@gmail.com>
2024-04-13 13:45:34 +0500

    applying while loop and update version

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ import inquirer from "inquirer";
 console.log("========================================================");
 console.log("                    Number Gussing Game                 ");
 console.log("========================================================");
-const NumberGussing = Math.floor(Math.random() * 6 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "UserGussNumber",
-        type: "number",
-        message: "Please Guess a Number Between 1-6 : "
-    },
-]);
-if (answer.UserGussNumber === NumberGussing) {
-    console.log("Congragulations you gusse a right number");
-}
-else {
-    console.log("Sorry! You Lost");
+let condition = true;
+while (condition) {
+    const NumberGussing = Math.floor(Math.random() * 6 + 1);
+    const answer = await inquirer.prompt([
+        {
+            name: "UserGussNumber",
+            type: "number",
+            message: "Please Guess a Number Between 1-6 : "
+        },
+    ]);
+    if (answer.UserGussNumber === NumberGussing) {
+        console.log("Congragulations you gusse a right number");
+        console.log("the correct number is: " + NumberGussing);
+    }
+    else {
+        console.log("Sorry! You Lost");
+        console.log("the correct number is: " + NumberGussing);
+    }
+    let cond = await inquirer.prompt([
+        {
+            name: "more",
+            type: "confirm",
+            message: "Do you want do more ?",
+            default: "false",
+        },
+    ]);
+    condition = cond.more;
 }
--- index.ts
+++ index.ts
@@ -6,6 +6,9 @@ console.log("========================================================");
 console.log("                    Number Gussing Game                 ");
 console.log("========================================================");
 
+let condition = true;
+while (condition){
+
 const NumberGussing = Math.floor(Math.random() * 6 + 1);
 
 const answer = await inquirer.prompt([
@@ -17,7 +20,20 @@ const answer = await inquirer.prompt([
 ]);
 
 if(answer.UserGussNumber === NumberGussing){
-    console.log("Congragulations you gusse a right number")
+    console.log("Congragulations you gusse a right number");
+    console.log("the correct number is: " + NumberGussing);
+
 } else{
-    console.log("Sorry! You Lost")
+    console.log("Sorry! You Lost");
+    console.log("the correct number is: " + NumberGussing);
+}
+let cond = await inquirer.prompt([
+    {
+      name: "more",
+      type: "confirm",
+      message: "Do you want do more ?",
+      default: "false",
+    },
+  ]);
+  condition = cond.more;
 }
